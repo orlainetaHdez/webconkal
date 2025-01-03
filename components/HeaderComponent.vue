33c929b2fe3840c9e20853cb8f91553268194a30
orlainetaHdez <jonathan.orlaineta@gmail.com>
2025-01-03 10:14:38 -0600

    cambios en header

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -72,7 +72,30 @@ function getCurrentDate() {
     <div style="min-height: 102.13px;background-color: #022452;overflow-x: hidden;display: grid">
             <div style="display: flex;align-items: center;justify-content: center;width: 100%;">
               <div class="row" style="width: 100%;max-width: 1155px;display: flex;justify-content: center;align-items: center;">
-                <div class="col-md-4" style="padding-right:0px; padding-left: 0px;">
+                
+                <div class="col-6 movil3" style="padding-right:0px; padding-left: 0px;display: none;">
+                  <div style="display: flex;justify-content: left;">
+                     <div style="justify-content: left;">
+                        <div class="team-icon d-none d-xl-flex justify-content-center me-3" style="padding-top:15px;display:flex !important;width: fit-content;">
+                                 <a href="#" class="btn btn-square btn-light rounded-circle mx-1" target="_blank" style="padding: 0;display: flex;align-items: center;justify-content: center;font-weight: normal;background-color: #CB2F80;"><i class="fab fa-facebook-f" style="color:#ffffff;width: 24px;"></i></a>
+                                  <!-- <a href="#" class="btn btn-square btn-light rounded-circle mx-1"  target="_blank"><i class="fab fa-twitter" style="color:#00496c;"></i></a> -->
+                                 <a class="btn btn-square btn-light rounded-circle mx-1" href=""  style="padding: 0;display: flex;align-items: center;justify-content: center;font-weight: normal;background-color: #CB2F80;"><i class="fab fa-instagram" style="color:#ffffff;width: 24px;"></i></a>
+                                 <a href="#" class="btn btn-square btn-light rounded-circle mx-1"  target="_blank"  style="padding: 0;display: flex;align-items: center;justify-content: center;font-weight: normal;background-color: #CB2F80;"><i class="fab fa-youtube" style="color:#ffffff;width: 24px;"></i></a>
+                                 <a href="#" class="btn btn-square btn-light rounded-circle mx-1"  target="_blank"  style="padding: 0;display: flex;align-items: center;justify-content: center;font-weight: normal;background-color: #CB2F80;"><i class="fab fa-tiktok" style="color:#ffffff;width: 24px;"></i></a>
+                        </div>
+                        <!-- <div class="busqueda-container" style="background-color: white;width: 177px; border-radius: 48px;height: 32px;margin-top: 10px;">
+                          <i class="fas fa-search icono-lupa" onclick="buscarTexto()"></i>
+                            <input type="text" id="busqueda" placeholder="Buscar..." style="width: 140px; border-radius: 48px;height: 32px;font-size: 12px; background-color: transparent;border-color: transparent;color: black;outline: none;">
+                        </div> -->
+                      </div>
+                  </div>
+                </div>
+                <div class="col-6 movil3" style="padding-bottom: 10px;text-align: right;padding-top: 10px;display: none;">
+                          <a href="/"><img src="assets/images/concal/Logotipo.png" alt="Logo" style="width: 100%;max-width: 239px;"> </a>
+                </div>
+                
+                
+                <div class="col-md-4 movil2" style="padding-right:0px; padding-left: 0px;">
                   <div style="display: flex;justify-content: left;">
                      <div style="justify-content: center;">
                         <div class="team-icon d-none d-xl-flex justify-content-center me-3" style="padding-top:15px;display:flex !important;width: fit-content;">
@@ -89,9 +112,13 @@ function getCurrentDate() {
                       </div>
                   </div>
                 </div>
+
+
+
+                
                  <div class="col-md-8" style="padding-right:0px; padding-left: 0px;">
                     <div class="row">
-                        <div class="col-md-5" style="padding-bottom: 10px;text-align: center;padding-top: 10px;">
+                        <div class="col-md-5 movil2" style="padding-bottom: 10px;text-align: center;padding-top: 10px;">
                           <a href="/"><img src="assets/images/concal/Logotipo.png" alt="Logo" style="width: 100%;max-width: 239px;"> </a>
                         </div>
                         <div class="col-md-7">
@@ -153,5 +180,12 @@ function getCurrentDate() {
             text-align:left; /* Alinear texto a la derecha */
         }
 
-     
+@media (max-width: 767px) {
+  .movil3{
+    display: block !important;
+  }
+  .movil2{
+    display: none;
+  }
+}
 </style>
